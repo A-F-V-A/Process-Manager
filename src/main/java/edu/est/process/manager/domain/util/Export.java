83c85f88ec462589d13432236731d91deab06369
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-11-18 22:36:00 -0500

    se organiza la clase export

--- src/main/java/edu/est/process/manager/domain/util/Export.java
+++ src/main/java/edu/est/process/manager/domain/util/Export.java
@@ -3,51 +3,43 @@ package edu.est.process.manager.domain.util;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import edu.est.process.manager.domain.models.*;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * Clase que gestiona la exportación de datos a archivos CSV y Excel.
  */
 public class Export {
 
-    /**
-     * Exporta los datos a un archivo CSV en la ruta especificada.
-     *
-     * @param data     Datos a exportar en forma de lista de matrices de cadenas.
-     * @param filePath Ruta del archivo CSV de destino.
-     * @throws IOException Si ocurre un error al escribir en el archivo CSV.
-     */
-    public static void exportToCSV(List<String[]> data, String filePath) throws IOException {
-        try (FileWriter writer = new FileWriter(filePath)) {
-            for (String[] row : data) {
-                for (int i = 0; i < row.length; i++) {
-                    writer.append(row[i]);
-                    if (i < row.length - 1) {
-                        writer.append(",");
-                    }
-                }
-                writer.append("\n");
-            }
-        }
-    }
+    public static void exportToExcel(String filePath) throws IOException {
+        Export export = new Export();
+        List<String[]> data = export.loadDataForExcel();
 
-    /**
-     * Exporta los datos a un archivo Excel en la ruta especificada.
-     *
-     * @param data     Datos a exportar en forma de lista de matrices de cadenas.
-     * @param filePath Ruta del archivo Excel de destino.
-     * @throws IOException Si ocurre un error al escribir en el archivo Excel.
-     */
-    public static void exportToExcel(List<String[]> data, String filePath) throws IOException {
-        Workbook workbook = null;
-        FileOutputStream fileOut = null;
-        try {
-            workbook = WorkbookFactory.create(true);
+        try (Workbook workbook = WorkbookFactory.create(true);
+             FileOutputStream fileOut = new FileOutputStream(filePath)) {
+
             Sheet sheet = workbook.createSheet("Datos");
 
-            // Escribir los datos en la hoja de cálculo
-            int rowNum = 0;
+            // Encabezados de las columnas
+            String[] headers = {
+                    "nombre proceso", "idProceso", "DescripcionProceso", "duracionProceso",
+                    "idActividades", "nombre actividad", "actividad", "nombreDescripcion",
+                    "idTarea", "descripcion", "estado", "duracion"
+            };
+            Row headerRow = sheet.createRow(0);
+            for (int i = 0; i < headers.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(headers[i]);
+            }
+
+            int rowNum = 1;
             for (String[] rowData : data) {
                 Row row = sheet.createRow(rowNum++);
                 int colNum = 0;
@@ -57,56 +49,73 @@ public class Export {
                 }
             }
 
-            // Guardar el archivo Excel si no existe previamente
-            fileOut = new FileOutputStream(filePath);
             workbook.write(fileOut);
+        }
+    }    public List<String[]> loadDataForExcel() {
+        List<String[]> excelData = new ArrayList<>();
 
-        } finally {
-            if (workbook != null) {
-                try {
-                    workbook.close();
-                } catch (IOException e) {
-                    // Manejar el cierre del workbook
-                    e.printStackTrace();
-                }
-            }
-            if (fileOut != null) {
-                try {
-                    fileOut.close();
-                } catch (IOException e) {
-                    // Manejar el cierre del FileOutputStream
-                    e.printStackTrace();
-                }
+        ProcessManager processManager = ProcessManager.getInstance();
+        Map<String, CustomProcess> processes = processManager.getProcesses();
+
+        for (CustomProcess process : processes.values()) {
+            excelData.add(getProcessInfo(process)); // Datos del proceso
+
+            // Datos de las actividades
+            for (Activity activity : process.getActivities().toList()) {
+                excelData.add(getActivityInfo(process, activity)); // Datos de la actividad
+                excelData.addAll(getPendingTaskInfo(process, activity)); // Datos de tareas pendientes
+                excelData.addAll(getCompletedTaskInfo(process, activity)); // Datos de tareas completadas
             }
         }
+
+        return excelData;
     }
 
-    /**
-     * Exporta los datos a un archivo especificado según el tipo de archivo deseado (CSV o Excel).
-     *
-     * @param data     Datos a exportar en forma de lista de matrices de cadenas.
-     * @param filePath Ruta del archivo de destino.
-     * @param fileType Tipo de archivo deseado (CSV o Excel).
-     * @throws IOException Si ocurre un error al escribir en el archivo.
-     */
-    public void exportData(List<String[]> data, String filePath, FileType fileType) throws IOException {
-        switch (fileType) {
-            case CSV:
-                exportToCSV(data, filePath);
-                break;
-            case EXCEL:
-                exportToExcel(data, filePath);
-                break;
-            default:
-                throw new IllegalArgumentException("Tipo de archivo no compatible");
+    private String[] getProcessInfo(CustomProcess process) {
+        return new String[]{
+                process.getName(),
+                process.getId(),
+                process.getDescription(),
+                String.valueOf(process.getTotalDurationMinutes())
+        };
+    }
+
+    private String[] getActivityInfo(CustomProcess process, Activity activity) {
+        return new String[]{
+                process.getName(), // Nombre del proceso al que pertenece la actividad
+                activity.getId(),
+                activity.getDescription(),
+        };
+    }
+
+    private List<String[]> getPendingTaskInfo(CustomProcess process, Activity activity) {
+        List<String[]> pendingTasksInfo = new ArrayList<>();
+        for (Task task : activity.getPendingTasksAsList()) {
+            String[] pendingTaskInfo = {
+                    process.getName(), // Nombre del proceso al que pertenece la tarea
+                    activity.getId(), // ID de la actividad a la que pertenece la tarea
+                    activity.getDescription(),
+                    task.getDescription(),
+                    String.valueOf(task.getStatus()),
+                    String.valueOf(task.getDurationMinutes())
+            };
+            pendingTasksInfo.add(pendingTaskInfo);
         }
+        return pendingTasksInfo;
     }
 
-    /**
-     * Enumeración que define los tipos de archivo soportados para la exportación.
-     */
-    public enum FileType {
-        CSV,
-        EXCEL
+    private List<String[]> getCompletedTaskInfo(CustomProcess process, Activity activity) {
+        List<String[]> completedTasksInfo = new ArrayList<>();
+        for (Task task : activity.getCompletedTasksAsList()) {
+            String[] completedTaskInfo = {
+                    process.getName(), // Nombre del proceso al que pertenece la tarea
+                    activity.getId(), // ID de la actividad a la que pertenece la tarea
+                    task.getDescription(),
+                    String.valueOf(task.getStatus()),
+                    String.valueOf(task.getDurationMinutes())
+            };
+            completedTasksInfo.add(completedTaskInfo);
+        }
+        return completedTasksInfo;
     }
 }
